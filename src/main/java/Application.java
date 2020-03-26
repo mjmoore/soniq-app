c244d7d0f5f41203b8421c6db3ce6b8eccc94dec
Martin Moore <martinmoorej@gmail.com>
2020-03-26 10:25:06 +0100

    Allows spring-boot to start up

---
+++ src/main/java/Application.java
@@ -0,0 +1,8 @@
+import org.springframework.boot.SpringApplication;
+
+public class Application {
+
+    public static void main(final String... args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
