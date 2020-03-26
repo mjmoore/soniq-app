a2e3eb7fac8eea8007f6908aede8d1262bce28a1
Martin Moore <martinmoorej@gmail.com>
2020-03-26 16:46:31 +0100

    Move spring boot applicaiton to a real package

--- src/main/java/io/mjmoore/Application.java
+++ src/main/java/io/mjmoore/Application.java
@@ -1,5 +1,9 @@
-import org.springframework.boot.SpringApplication;
+package io.mjmoore;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
 
     public static void main(final String... args) {
